576e74237375872cc81127a3ad908dcf3f3aa1cb
David Foote <david.a.foote@gmail.com>
2019-06-13 18:26:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,7 @@ def prime?(num)
   if num == 1 || num == 0 || num < 0
     return false
   end
-  # (2..num).to_a
+  (2..num).to_a.all? do |possible_factor| 
+    num % possible_factor != 0
+  end
 end
